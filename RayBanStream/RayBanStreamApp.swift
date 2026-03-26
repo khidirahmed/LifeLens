926511bb12125d25ceaa8d66b303aa61854bf232
Khidir Ahmed <kka6822@nyu.edu>
2026-03-26 15:11:45 -0400

    Initial Commit

---
+++ RayBanStream/RayBanStreamApp.swift
@@ -0,0 +1,32 @@
+//
+//  RayBanStreamApp.swift
+//  RayBanStream
+//
+//  Created by Khidir Ahmed on 3/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RayBanStreamApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RayBanStreamTests/RayBanStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  RayBanStreamTests.swift
+//  RayBanStreamTests
+//
+//  Created by Khidir Ahmed on 3/26/26.
+//
+
+import Testing
+@testable import RayBanStream
+
+struct RayBanStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
